ec26e065f5f96f6ddb5097976180a49d253c43c0
jMyles <justin@justinholmes.com>
2025-01-23 21:52:26 -0500

    Fixes for better jenkins persistence and failure modes.

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,15 @@
 pipeline {
     agent any
     
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10'))
+    }
+
     environment {
         NODE_VERSION = '23.2.0'
         NVM_DIR = '/var/jenkins_home/.nvm'
+        ALCHEMY_API_KEY = credentials('ALCHEMY_API_KEY')
+        DISCORD_BOT_TOKEN = credentials('DISCORD_BOT_TOKEN')
     }
     
     stages {
@@ -33,11 +39,14 @@ pipeline {
         
         stage('Install Dependencies') {
             steps {
+                // Only install if node_modules doesn't exist or package.json changed
                 sh '''
-                    export NVM_DIR="$NVM_DIR"
-                    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-                    nvm use ${NODE_VERSION}
-                    npm install
+                    if [ ! -d "node_modules" ] || [ package.json -nt node_modules ]; then
+                        export NVM_DIR="$NVM_DIR"
+                        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+                        nvm use ${NODE_VERSION}
+                        npm install
+                    fi
                 '''
             }
         }
@@ -75,10 +84,23 @@ pipeline {
             }
         }
         
-        stage('Build Sites') {
+        stage('Build cryptograss.live') {
             steps {
                 sh '''
                     export NVM_DIR="$NVM_DIR"
+                    export SITE=cryptograss.live
+                    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+                    nvm use ${NODE_VERSION}
+                    npm run build
+                '''
+            }
+        }
+
+        stage('Build justinholmes.com') {
+            steps {
+                sh '''
+                    export NVM_DIR="$NVM_DIR"
+                    export SITE=justinholmes.com
                     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
                     nvm use ${NODE_VERSION}
                     npm run build
--- src/build_logic/webpack.prod.js
+++ src/build_logic/webpack.prod.js
@@ -1,54 +1,52 @@
-import jhCommon from './webpack.justinholmes.common.js';
-import cgCommon from './webpack.cryptograss.common.js';
-import {merge} from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import TerserPlugin from 'terser-webpack-plugin';
 import webpack from 'webpack';
-import { outputjhcomDistDir, outputcryptograssDistDir, templateDir } from "./constants.js";
+import { getProjectDirs } from './locations.js'
 import fs from "fs";
 import path from "path";
 
-const prodConfig = {
-    mode: 'production',
-    devtool: false,
-    optimization: {
-        minimizer: [new TerserPlugin()],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-        }),
-        {
-            apply: (compiler) => {
-                compiler.hooks.done.tap('CopyHtaccessPlugin', () => {
-                    // Copy .htaccess for both sites
-                    fs.copyFileSync(
-                        path.resolve(templateDir, 'shared/.htaccess'),
-                        path.resolve(outputjhcomDistDir, '.htaccess')
-                    );
-                    fs.copyFileSync(
-                        path.resolve(templateDir, 'shared/.htaccess'),
-                        path.resolve(outputcryptograssDistDir, '.htaccess')
-                    );
-                    console.log('.htaccess files copied');
-                });
-            },
-        },
-    ]
-};
+// Dynamically import the common webpack config for the project, depending on the site argument
+const site = process.env.SITE;
 
-export default [
-    merge(jhCommon, {
-        ...prodConfig,
-        output: {
+export async function generateProductionConfig() {
+    let common;
+
+    if (site === 'justinholmes.com') {
+        common = await import('./webpack.justinholmes.common.js');
+    } else if (site === 'cryptograss.live') {
+        common = await import('./webpack.cryptograss.common.js');
+    } else {
+        throw new Error('Invalid site argument');
+    }
+
+    const { templateDir, outputDistDir } = getProjectDirs();
+
+    return merge(common['default'], {
+        mode: 'production',
+        devtool: false,
+        optimization: {
+            minimizer: [new TerserPlugin()],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production'),
+            }),
+            {
+                apply: (compiler) => {
+                    compiler.hooks.done.tap('CopyHtaccessPlugin', () => {
+                        fs.copyFileSync(
+                            path.resolve(templateDir, 'shared/.htaccess'),
+                            path.resolve(outputDistDir, '.htaccess')
+                        );
+                        console.log('.htaccess files copied');
+                    });
+                },
+            },
+        ], output: {
             filename: '[name].bundle.js',
-            path: outputjhcomDistDir,
+            path: outputDistDir,
         }
-    }),
-    merge(cgCommon, {
-        ...prodConfig,
-        output: {
-            filename: '[name].bundle.js',
-            path: outputcryptograssDistDir,
-        }
-    })
-];
+    });
+}
+
+export default generateProductionConfig;
