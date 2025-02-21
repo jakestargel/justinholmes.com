33b2f061216840b4a7a155e11fb5f07089f8a57b
jMyles <justin@justinholmes.com>
2025-02-20 18:40:56 -0700

    Happenings!

--- src/sites/cryptograss.live/js/index.js
+++ src/sites/cryptograss.live/js/index.js
@@ -1,2 +1,9 @@
 // What is this file for, really?  It's just a placeholder.
 import '../styles/styles-common.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as popper from 'popper.js';
+import { Dropdown } from 'bootstrap';
+import * as bootstrap from 'bootstrap';
+console.log("Here's some things");
+console.log(popper);
+console.log(Dropdown);
